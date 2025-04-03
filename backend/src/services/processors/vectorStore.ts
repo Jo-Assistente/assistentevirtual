c6c5a9e39dbfb7f7bc92dcf097b66a069c8ca2f2
Arthur <arthurbarreiros.dev@gmail.com>
2025-04-03 07:31:53 -0300

    versao call center sprint

--- backend/src/services/processors/vectorStore.ts
+++ backend/src/services/processors/vectorStore.ts
@@ -9,7 +9,7 @@ export const similarChunks = async (userQuery: string): Promise<string> => {
     path: "http://chromadb:8000",
   });
   const collection = await client.getOrCreateCollection({
-    name: "mvp-jo",
+    name: "jo-call-center-sprint",
     embeddingFunction: new OpenAIEmbeddingFunction({
       openai_api_key: process.env.OPENAI_API_KEY as string,
     }),
